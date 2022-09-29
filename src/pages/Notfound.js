637d99960c4e4b5253403d7a357773672c47a5de
bondonflorian <101095119+bondonflorian@users.noreply.github.com>
2022-09-29 18:55:33 +0200

    Mise en page de l'erreur 404

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Notfound = () => {
     return (
-        <div>
-            <h1>Error</h1>
+        <div className='notfound'>
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <NavLink to="/" className="notfound-href">Retourner sur la page d'accueil</NavLink> {/* TODO Définir le soulignement du text */}
         </div>
     );
 };
