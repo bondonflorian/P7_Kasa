54a9f6da76ef05ea680f2527ab166e2efc4a12c3
bondonflorian <101095119+bondonflorian@users.noreply.github.com>
2022-10-11 10:31:36 +0200

    Correction mise en page responsive mobile de la page home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,12 +12,12 @@ const Home = () => {
             </div>
             <div className="card">
                 <div className="test">
-                    <Link to="/housing1" >
-                        <div className="card__location">
+                    <div className="card__location">
+                        <Link to="/housing1" >
                             {/* <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="" /> */}
                             <h2>Titre de la location</h2>
-                        </div>
-                    </Link>
+                        </Link>
+                    </div>
                     {/* TODO Element temporaire pour la mise en page */}
                     <div className="card__location">
                         <h2>Titre de la location</h2>
