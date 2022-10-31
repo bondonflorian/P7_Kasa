1f96b1bb8429ecdd064d8e8ad144fb857263aa89
bondonflorian <101095119+bondonflorian@users.noreply.github.com>
2022-10-31 16:19:29 +0100

    Correction général et mis en place d'une condition dans le composant collapse

--- src/components/layout/Collapse.js
+++ src/components/layout/Collapse.js
@@ -1,27 +1,36 @@
 import React, { useState, useRef } from "react";
+import { useLocation } from 'react-router-dom';
 const arrowup = require("../../assets/img/arrow_up.png");
 const arrowdown = require("../../assets/img/arrow_down.png");
 
 const Collapse = (props) => {
-    const [setActive, setActiveState] = useState("");
-    const [setHeight, setHeightState] = useState("0px");
+    const [active, setActive] = useState("");
+    const [height, setHeight] = useState("0px");
+    const [heighthousing, setHeightHousing] = useState("0px");
+    const { pathname } = useLocation();
 
     const content = useRef(null);
 
     const stateCollapse = () => {
-        setActiveState(setActive === "" ? "active" : "");
-        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
+        setActive(active === "" ? "open" : "");
+        setHeight(active === "open" ? "0px" : `${content.current.scrollHeight}px`);
+        setHeightHousing(active === "open" ? "0px" : "250px");
     }
 
     return (
         <>
-            <div className={`${props.className} ${setActive}`} onClick={stateCollapse}>
+            <div className={`${props.className} ${active}`} onClick={stateCollapse}>
                 <h1>{props.title}</h1>
-                {(setActive ? <img src={arrowup} alt="Flêche du haut" className='collapse__arrow' /> : <img src={arrowdown} alt="Flêche du bas" className='collapse__arrow' />)}
+                {(active ? <img src={arrowup} alt="Flêche du haut" className='collapse__arrow' /> : <img src={arrowdown} alt="Flêche du bas" className='collapse__arrow' />)}
             </div>
-            <div ref={content} className="collapse__description" style={{ maxHeight: `${setHeight}` }}>
-                <p className="collapse__description__show">{props.content}</p>
-            </div>
+            {pathname === "/about" ?
+                <div ref={content} className="collapse__description" style={{ maxHeight: `${height}` }}>
+                    <p className="collapse__description__show">{props.content}</p>
+                </div>
+                :
+                <div ref={content} className="collapse__description__housing" style={{ maxHeight: `${heighthousing}` }}>
+                    <p className="collapse__description__show">{props.content}</p>
+                </div>}
         </>
     );
 }
